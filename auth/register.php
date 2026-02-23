1f33d5916658a2f45cf1934330116fc30f7352b6
unknown <Fakhri>
2026-02-23 15:43:34 +0700

    memulai ulang

---
+++ auth/register.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Sekarang Jugaa</title>
+</head>
+
+<body>
+    <div class="form_register">
+        <form action="function.php" method="post">
+            <table>
+                <tr>
+                    <td>Nama</td>
+                    <td> : </td>
+                    <td><input type="text" name="name" id="name"></td>
+                </tr>
+
+                <tr>
+                    <td>Email</td>
+                    <td> : </td>
+                    <td><input type="email" name="email" id="email"></td>
+                </tr>
+
+                <tr>
+                    <td>Password</td>
+                    <td> : </td>
+                    <td><input type="password" name="password" id="password"></td>
+                </tr>
+            </table>
+            <button type="submit">Daftar</button>
+        </form>
+    </div>
+</body>
+
+</html>
